d06338b200402a2c9ce8b751ea43bd2fd3ce5b62
brandon-ouellette <b.ouellette@snhu.edu>
2022-01-20 10:55:59 -0500

    Began work on section 11

---
+++ Exercises/exercises-chapter-11.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+#Practice 11.2.2.1
+#you would use read_delim() to read a file with "|" as the delimiter.
+
+#Practice 11.2.2.2
+#read_csv() and read_tsv() share many of the same arguments. they perform very similar functions on different types of files.
+
+#Practice 11.2.2.3
